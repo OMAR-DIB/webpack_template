56f9491ee26dd8e6cbb44de7c4556dfbb58ae82c
OMAR-DIB <omardib759@gmail.com>
2024-11-17 20:30:50 +0200

    Initial Webpack template setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import odinImage from "../assets/odin-lined.png";
+// import { greeting } from "./greeting.js";
+
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
